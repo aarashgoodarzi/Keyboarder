fdfcf956c49a4fe381bd52bfdbaa33e5446d410d
test <arash.good93@gmail.com>
2020-01-16 13:33:14 -0800

    sample project added

---
+++ KeyboarderSample/KeyboarderSample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  KeyboarderSample
+//
+//  Created by Arash Goodarzi on 1/16/20.
+//  Copyright © 2020 Arash Goodarzi. All rights reserved.
+//
+
+import UIKit
+import Keyboarder
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Keyboarder.shared.didHide { (keyboardHeight) in
+            //TODO: dance
+        }
+        Keyboarder.shared.toolbarDoneBtnAction {
+            print("actionnn")
+        }
+    }
+
+
+}
+
